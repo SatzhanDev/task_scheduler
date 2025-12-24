9776901eb2f4f44a69c842f3ace4984672b205a4
SatzhanDev <113493666+SatzhanDev@users.noreply.github.com>
2025-12-24 09:45:20 +0500

    feat: added List func

--- internal/task/service.go
+++ internal/task/service.go
@@ -14,6 +14,7 @@ var (
 type Service interface {
 	Create(title string, dueAt *time.Time) (*Task, error)
 	Get(id int) (*Task, error)
+	List(limit, offset int) ([]Task, int, error)
 }
 
 type TaskService struct {
@@ -56,6 +57,49 @@ func (s *TaskService) Get(id int) (*Task, error) {
 	}
 	return task, nil
 
+}
+
+func (s *TaskService) List(limit, offset int) ([]Task, int, error) {
+	// 1. Валидация offset
+
+	if offset < 0 {
+		return nil, 0, ErrInvalidInput
+	}
+
+	// 2. Значения по умолчанию и ограничения
+	if limit <= 0 {
+		limit = 20
+	}
+	if limit > 100 {
+		limit = 100
+	}
+
+	// 3. Блокируем доступ к данным
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// 4. Сколько всего задач
+	total := len(s.tasks)
+
+	// 5. Если offset больше total — просто вернём пустой список
+	if offset >= total {
+		return []Task{}, total, nil
+	}
+
+	// 6. Преобразуем map -> slice
+	all := make([]Task, 0, total)
+	for _, t := range s.tasks {
+		all = append(all, *t)
+	}
+
+	// 7. Вычисляем границы
+	end := offset + limit
+	if end > total {
+		end = total
+	}
+
+	// 8. Возвращаем нужный кусок
+	return all[offset:end], total, nil
 }
 
 func NewService() Service {
