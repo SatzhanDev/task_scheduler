8200525bc5bc215c1472e0d6df7fde6c8ada9328
SatzhanDev <113493666+SatzhanDev@users.noreply.github.com>
2025-12-26 15:52:06 +0500

    feat: deleted comments

--- internal/task/service.go
+++ internal/task/service.go
@@ -17,9 +17,6 @@ type Service interface {
 }
 
 type TaskService struct {
-	// mu     sync.Mutex
-	// nextID int
-	// tasks  map[int]*Task
 	repo Repo
 }
 
@@ -33,11 +30,6 @@ func (s *TaskService) Create(title string, dueAt *time.Time) (*Task, error) {
 	if title == "" {
 		return nil, ErrInvalidInput
 	}
-	// s.mu.Lock()
-	// defer s.mu.Unlock()
-
-	// id := s.nextID
-	// s.nextID++
 	now := time.Now().UTC()
 	task := &Task{
 		Title:     title,
@@ -46,7 +38,6 @@ func (s *TaskService) Create(title string, dueAt *time.Time) (*Task, error) {
 		CreatedAt: now,
 		UpdatedAt: now,
 	}
-	// s.tasks[id] = task
 	if err := s.repo.Create(task); err != nil {
 		return nil, err
 	}
@@ -57,12 +48,7 @@ func (s *TaskService) Get(id int) (*Task, error) {
 	if id <= 0 {
 		return nil, ErrInvalidInput
 	}
-	// s.mu.Lock()
-	// defer s.mu.Unlock()
-	// task, ok := s.tasks[id]
-	// if !ok {
-	// 	return nil, ErrNotFound
-	// }
+
 	task, err := s.repo.Get(id)
 	if err != nil {
 		return nil, err
@@ -92,35 +78,4 @@ func (s *TaskService) List(limit, offset int) ([]Task, int, int, error) {
 	}
 	return tasks, total, limit, nil
 
-	// 3. Блокируем доступ к данным
-	// s.mu.Lock()
-	// defer s.mu.Unlock()
-
-	// 4. Сколько всего задач
-	// total := len(s.tasks)
-
-	// 5. Если offset больше total — просто вернём пустой список
-	// if offset >= total {
-	// 	return []Task{}, total, limit, nil
-	// }
-
-	// // 6. Преобразуем map -> slice
-	// all := make([]Task, 0, total)
-	// for _, t := range s.tasks {
-	// 	all = append(all, *t)
-	// }
-
-	// //7. Сортировка. Бывает только при in memory памяти когда используем map.
-	// // При SQL database так не будет
-	// sort.Slice(all, func(i, j int) bool {
-	// 	return all[i].ID < all[j].ID
-	// })
-	// // 8. Вычисляем границы
-	// end := offset + limit
-	// if end > total {
-	// 	end = total
-	// }
-
-	// 9. Возвращаем нужный кусок
-	// return all[offset:end], total, limit, nil
 }
